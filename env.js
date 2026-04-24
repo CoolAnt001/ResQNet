2543a7560e025551b25487af2bf1098a49b9b64f
Amartya Bhaumik <155053095+CoolAnt001@users.noreply.github.com>
2026-04-24 16:58:51 +0530

    Update env.js

--- env.js
+++ env.js
@@ -7,5 +7,5 @@ const ENV = {
     FIREBASE_MESSAGING_SENDER_ID: "425282005905",
     FIREBASE_APP_ID: "1:425282005905:web:a6a9722a7e802b7d8e4830",
     LOCAL_ENDPOINT: "http://10.190.35.195:8080",
-    TUNNEL_ENDPOINT: "https://licensed-employed-cbs-really.trycloudflare.com"
+    TUNNEL_ENDPOINT: "https://absence-bulldog-haiku.ngrok-free.dev"
 };
